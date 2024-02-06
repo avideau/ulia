81d43a8cf4fd59b4ce4a48e079c0a54ac83047b5
Antoine Videau <avideau@manager.one>
2024-02-06 18:10:32 +0100

    remove node

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const gttp = require('http');
 const httpProxy = require('http-proxy');
 const https = require('https');
 
-httpProxy.createProxyServer({target:'http://localhost:5173'}).listen(8000);
+httpProxy.createProxyServer({target:'http://localhost:4455'}).listen(8000);
